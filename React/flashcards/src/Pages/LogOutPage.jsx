a90efcf8fd9a075bfd92adddc65bdd394b3cc2af
Jessica <justinholc18@gmail.com>
2021-02-19 10:42:57 -0800

    made a log in page

---
+++ React/flashcards/src/Pages/LogOutPage.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class LogOutPage extends Component{
+    render() {
+        return(
+            <div className = "logout">
+                <h1>LOGOUT PAGE</h1>
+            </div>
+        )
+    }
+}
+export default LogOutPage
